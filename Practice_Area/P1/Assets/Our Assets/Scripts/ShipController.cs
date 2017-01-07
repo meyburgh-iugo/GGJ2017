9befbfd2fcef9af1bf2b40fa588685dedf74c054
Albert Meyburgh <albert.meyburgh@iugome.com>
2017-01-07 13:48:00 -0800

    add a little ship thing

---
+++ Practice_Area/P1/Assets/Our Assets/Scripts/ShipController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ShipController : MonoBehaviour {
+  Rigidbody2D rb;
+  readonly float force_multiplier = 20;
+
+  // Use this for initialization
+  void Start () {
+    rb = GetComponent<Rigidbody2D>();
+	}
+	
+  void FixedUpdate()
+  {
+    rb.AddRelativeForce(new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")).normalized * force_multiplier);
+    
+  }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
