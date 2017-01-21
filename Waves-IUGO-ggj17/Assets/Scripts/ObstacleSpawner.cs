85f1146802243ae9a1efbe3f94d4a14f8409c120
Albert Meyburgh <albert.meyburgh@iugome.com>
2017-01-21 04:48:28 -0800

    randomize the soup, and give it very large mass so our sonar pulses don’t move them

--- Waves-IUGO-ggj17/Assets/Scripts/ObstacleSpawner.cs
+++ Waves-IUGO-ggj17/Assets/Scripts/ObstacleSpawner.cs
@@ -6,7 +6,7 @@ public class ObstacleSpawner : MonoBehaviour {
 
   public GameObject[] primitives;
   public int objectCount = 200;
-  public int density = 10;
+  public int density = 15;
 
 	// Use this for initialization
 	void Start ()
@@ -27,6 +27,10 @@ public class ObstacleSpawner : MonoBehaviour {
   {
     var go = Instantiate(primitives[Random.Range(0, primitives.Length - 1)], new Vector2(Random.Range(-objectCount/ density, objectCount/ density), Random.Range(-objectCount/ density, objectCount/ density)), Quaternion.identity);
     go.transform.localScale = new Vector3(Random.Range(0.25f, 10), Random.Range(0.25f, 10), 1);
+    go.transform.localEulerAngles = new Vector3(0, 0, Random.Range(0, 360));
     go.transform.parent = transform;
+    Rigidbody2D body = go.AddComponent<Rigidbody2D> ();
+    body.AddForce (new Vector2 (Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f)), ForceMode2D.Impulse);
+    body.mass = 1000000;
   }
 }
