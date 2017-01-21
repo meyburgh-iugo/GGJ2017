af1787bb25e2fa6a6faaef11455f0ad30c51cf96
Sergio Nunes <sjuniorhp@gmail.com>
2017-01-21 05:53:27 -0800

    Player Movement

--- Waves-IUGO-ggj17/Assets/Scripts/Compass.cs
+++ Waves-IUGO-ggj17/Assets/Scripts/Compass.cs
@@ -7,16 +7,15 @@ public class Compass : MonoBehaviour
   private Transform t;
   public Rigidbody2D player;
   public Transform goal;
- 
 	// Use this for initialization
 	void Awake ()
   {
-    t = transform;	
+    t = transform;
 	}
 	
 	// Update is called once per frame
 	void Update ()
   {
-    t.localRotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(goal.position.y - player.position.y, goal.position.x - player.position.x)); 	
-	}
+    t.localRotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(goal.position.y - player.position.y, goal.position.x - player.position.x));
+  }
 }
---
+++ Waves-IUGO-ggj17/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  private Rigidbody2D rb;
+  private Transform t;
+	public float speed = 10;
+  public float turnSpeed = 100;
+  // Use this for initialization
+	void Awake ()
+  {
+    t = transform;
+    rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+  {
+    var vert = Input.GetAxis("Vertical");
+    var hori = Input.GetAxis("Horizontal");
+
+    rb.rotation -= hori * Time.deltaTime * speed;
+
+    rb.velocity = new Vector2(t.right.x * speed * Time.deltaTime * vert, t.right.y * speed * Time.deltaTime * vert);
+  }
+}
