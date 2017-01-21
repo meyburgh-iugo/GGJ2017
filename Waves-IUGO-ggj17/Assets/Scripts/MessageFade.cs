4f35b67971c0faea22b66efded922dbf72a1ca73
Sergio Nunes <sjuniorhp@gmail.com>
2017-01-21 07:13:33 -0800

    Message Pooler

---
+++ Waves-IUGO-ggj17/Assets/Scripts/MessageFade.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageFade : MonoBehaviour
+{
+  public event Action OnFinishFade;
+  public float FadeInTime;
+  public float ShowTime;
+  public float FadeOutTime;
+  public Text text;
+
+  private string message;
+  private bool isPlaying = false;
+  public bool IsPlaying { get { return isPlaying;  } }
+
+  public void Awake()
+  {
+    var c = text.color;
+    text.color = new Color(c.r, c.g, c.b, 0);
+  }
+
+  public void ShowMessage(string _message)
+  {
+    message = _message;
+    StartCoroutine(FadeIn());
+  }
+
+  IEnumerator FadeIn()
+  {
+    isPlaying = true;
+    text.text = message;
+    float fade = 0.01f;
+    Color c = text.color;
+    while (fade <= FadeInTime)
+    {
+      text.color = new Color(c.r, c.g, c.b, fade / FadeInTime);
+      fade += Time.deltaTime;
+      yield return new WaitForEndOfFrame();
+    }
+
+    yield return new WaitForSeconds(ShowTime);
+
+    fade = FadeOutTime;
+    while (fade > 0)
+    {
+      text.color = new Color(c.r, c.g, c.b, fade / FadeOutTime);
+      fade -= Time.deltaTime;
+      yield return new WaitForEndOfFrame();
+    }
+
+    yield return new WaitForSeconds(ShowTime);
+    isPlaying = false;
+    if (OnFinishFade != null)
+    {
+      OnFinishFade();
+    }
+  }
+}
---
+++ Waves-IUGO-ggj17/Assets/Scripts/MessagePooler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagePooler : Singleton<MonoBehaviour>
+{
+  public string[] messages;
+  public float messageLong;
+  public Queue<string> messagesQueue;
+
+  private MessageFade fade;
+
+  private void Awake()
+  {
+    messagesQueue = new Queue<string>();
+  }
+
+  void Start ()
+  {
+    fade = GetComponent<MessageFade>();
+    fade.OnFinishFade += UnstackMessage;
+
+		for (int i = 0; i < messages.Length; i++)
+    {
+      messagesQueue.Enqueue(messages[i]);
+    }
+
+    if (messagesQueue.Count > 0)
+    {
+      fade.ShowMessage(messagesQueue.Dequeue());
+    }
+  }
+
+  void StackMessage(string message)
+  {
+    messagesQueue.Enqueue(message);
+    if (!fade.IsPlaying)
+    {
+      fade.ShowMessage(messagesQueue.Dequeue());
+    }
+  }
+	
+	// Update is called once per frame
+	void UnstackMessage ()
+  {
+    var message = messagesQueue.Dequeue();
+    if (message != null)
+    {
+      fade.ShowMessage(message);
+    }
+	}
+}
