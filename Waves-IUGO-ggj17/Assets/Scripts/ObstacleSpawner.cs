5d32141efef401dd1bccc9fdb7f9907029a71a32
Sergio Nunes <sjuniorhp@gmail.com>
2017-01-21 04:17:17 -0800

    Obstacle Spawner WIP. Photon Spawner WIP.

---
+++ Waves-IUGO-ggj17/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+  public GameObject[] primitives;
+  public int objectCount = 200;
+  public int density = 10;
+
+	// Use this for initialization
+	void Start ()
+  {
+	  for (int i = 0; i < objectCount; i++)
+    {
+      SpawnerARandomObstacle();
+    }  	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+		
+	}
+
+  void SpawnerARandomObstacle()
+  {
+    var go = Instantiate(primitives[Random.Range(0, primitives.Length - 1)], new Vector2(Random.Range(-objectCount/ density, objectCount/ density), Random.Range(-objectCount/ density, objectCount/ density)), Quaternion.identity);
+    go.transform.localScale = new Vector3(Random.Range(0.25f, 10), Random.Range(0.25f, 10), 1);
+    go.transform.parent = transform;
+  }
+}
--- Waves-IUGO-ggj17/Assets/Scripts/Photon.cs
+++ Waves-IUGO-ggj17/Assets/Scripts/Photon.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class Photon : MonoBehaviour
 {
-  public void Setup(Vector2 dir, float lifeSpan, float speed)
+  private SpriteRenderer sprite;
+  private Rigidbody2D rb;
+  private float alpha;
+  private float lifeSpan;
+  private Vector3 color;
+
+  private void Awake()
   {
-    GetComponent<Rigidbody2D>().velocity = dir.normalized * speed;
-    Invoke("Die", lifeSpan);
+    color = new Vector3(0, 0, 0);
+    sprite = GetComponent<SpriteRenderer>();
+    rb = GetComponent<Rigidbody2D>();
+    sprite.color = new Color(color.x, color.z, color.y, alpha);
   }
-	
-	// Update is called once per frame
-	void Die()
+
+  private void Update()
+  {
+    alpha -= Time.deltaTime * 1.0f/lifeSpan;
+    sprite.color = new Color(color.x, color.z, color.y, alpha);
+  }
+
+  public void Setup(Vector2 dir, float _lifeSpan, float speed)
+  {
+    lifeSpan = _lifeSpan;
+    rb.velocity = dir.normalized * speed;
+    Invoke("Die", lifeSpan);
+    alpha = 1.0f;
+  }
+
+  public void OnCollisionEnter2D(Collision2D collision)
+  {
+    float r = Random.Range(0.0f, 1.0f);
+    if (r > 0.7f)
+    {
+      color = new Vector3(0, 0.6f, 0);
+      rb.velocity = Vector2.zero; // -collision.relativeVelocity.normalized;
+    }
+  }
+
+  // Update is called once per frame
+  void Die()
   {
     Destroy(gameObject);
 	}
