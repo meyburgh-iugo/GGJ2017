58813ab1a10258a35a3e2e0d966ee8882b8ef75b
Albert Meyburgh <albert.meyburgh@iugome.com>
2017-01-21 08:15:38 -0800

    if an obstacle gets too far away from the player, nudge it in the direction of the player

--- Waves-IUGO-ggj17/Assets/Scripts/ObstacleSpawner.cs
+++ Waves-IUGO-ggj17/Assets/Scripts/ObstacleSpawner.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour {
 
   public GameObject[] primitives;
-  public int objectCount = 200;
-  public int density = 15;
+  public int objectCount = 50;
+  private int counter = 0;
+  public Transform goal;
+  private int maxDistance = 30;
 
 	// Use this for initialization
 	void Start ()
@@ -14,23 +16,30 @@ public class ObstacleSpawner : MonoBehaviour {
 	  for (int i = 0; i < objectCount; i++)
     {
       SpawnerARandomObstacle();
-    }  	
+    }
+
+    InvokeRepeating ("SpawnerARandomObstacle", 1.0f, 5.0f);
 	}
 	
-	// Update is called once per frame
-	void Update ()
-  {
-		
-	}
-
   void SpawnerARandomObstacle()
   {
-    var go = Instantiate(primitives[Random.Range(0, primitives.Length)], new Vector2(Random.Range(-objectCount/ density, objectCount/ density), Random.Range(-objectCount/ density, objectCount/ density)), Quaternion.identity);
+    var go = Instantiate(primitives[Random.Range(0, primitives.Length)], new Vector2(Random.Range(-maxDistance, maxDistance), Random.Range(-maxDistance, maxDistance)), Quaternion.identity);
     go.transform.localScale = new Vector3(Random.Range(0.25f, 10), Random.Range(0.25f, 10), 1);
     go.transform.localEulerAngles = new Vector3(0, 0, Random.Range(0, 360));
     go.transform.parent = transform;
     Rigidbody2D body = go.AddComponent<Rigidbody2D> ();
     body.AddForce (new Vector2 (Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f)), ForceMode2D.Impulse);
     body.mass = 1000000;
+  }
+
+  void Update()
+  {
+    ++counter;
+    int index = counter % objectCount;
+
+    if ((primitives[index].transform.position - goal.position).magnitude > maxDistance) {
+      Rigidbody2D body = primitives [index].GetComponent<Rigidbody2D> ();
+      body.AddForce ((goal.position - primitives[index].transform.position).normalized, ForceMode2D.Impulse);
+    }
   }
 }
