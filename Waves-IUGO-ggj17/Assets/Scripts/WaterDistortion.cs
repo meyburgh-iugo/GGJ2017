6373e5e8aa7e1c6b068547326fc4ca2f22ee2993
J-rod <jarrodmky@gmail.com>
2017-01-22 10:47:08 -0800

    added water shader, but it's not working

---
+++ Waves-IUGO-ggj17/Assets/Scripts/WaterDistortion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+// This class implements simple ghosting type Motion Blur.
+// If Extra Blur is selected, the scene will allways be a little blurred,
+// as it is scaled to a smaller resolution.
+// The effect works by accumulating the previous frames in an accumulation
+// texture.
+namespace UnityStandardAssets.ImageEffects
+{
+  [RequireComponent(typeof(Camera))]
+  public class WaterDistortion : MonoBehaviour
+  {
+    [Range(0.1f, 1.0f)]
+    public float WaveLength = 0.8f;
+
+    /// Provides a shader property that is set in the inspector
+    /// and a material instantiated from the shader
+    public Shader shader;
+
+    private Material material;
+
+    protected virtual void Start()
+    {
+      material = new Material(shader);
+
+      // Disable if we don't support image effects
+      if (!SystemInfo.supportsImageEffects)
+      {
+        enabled = false;
+        return;
+      }
+
+      // Disable the image effect if the shader can't
+      // run on the users graphics card
+      if (!shader || !shader.isSupported)
+        enabled = false;
+    }
+
+    void OnDisable()
+    {
+      if (material)
+      {
+        DestroyImmediate(material);
+      }
+    }
+
+    // Called by camera to apply image effect
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+      //clampo wave size
+      WaveLength = Mathf.Clamp(WaveLength, 0.1f, 1.0f);
+
+      // Setup the texture and floating point values in the shader
+
+      // Render the image using the water shader
+      //RenderTexture savedTex = Instantiate(source);
+
+      material.SetTexture("InTexture", source);
+      material.SetFloat("WaveLength", WaveLength);
+
+      Graphics.Blit(source, destination, material);
+    }
+  }
+}
---
+++ Waves-IUGO-ggj17/Assets/Shaders/WaterDistortionShader.shader
@@ -0,0 +1,58 @@
+﻿Shader "Custom/WaterDistortionShader" {
+
+	Properties{
+		_MyTexture("My Texture", 2D) = "white" { }
+	// other properties like colors or vectors go here as well
+	}
+
+
+
+		SubShader{
+
+		// Grab the screen behind the object into _BackgroundTexture
+		GrabPass
+	{
+		"_BackgroundTexture"
+	}
+
+		Pass{
+		// ... the usual pass state setup ...
+
+		CGPROGRAM
+// Upgrade NOTE: excluded shader from DX11; has structs without semantics (struct v2f members grabPos)
+#pragma exclude_renderers d3d11
+		// compilation directives for this snippet, e.g.:
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.cginc"
+
+		struct v2f
+	{
+		float4 grabPos ;
+		float4 pos : SV_POSITION;
+	};
+
+	float2 vert(appdata_base v) : TEXCOORD0 {
+		v2f o;
+
+		o.pos = UnityObjectToClipPos(v.vertex);
+
+		o.grabPos = ComputeGrabScreenPos(o.pos);
+		return o;
+	}
+
+	sampler2D _BackgroundTexture;
+
+	float4 frag(v2f i) : SV_Target
+	{
+		//just add u offset, should calculate sine
+		return tex2D(_BackgroundTexture, i.grabPos + float(i.y, 0));
+		//return 1 - bgcolor;
+	}
+		ENDCG
+		// ... the rest of pass setup ...
+	}
+
+
+	}
+}
