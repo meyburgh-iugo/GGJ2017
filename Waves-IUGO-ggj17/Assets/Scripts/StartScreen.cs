eaa9dabb81fac9c316d79dffb5db731a0093ad89
J-rod <jarrodmky@gmail.com>
2017-01-22 13:23:16 -0800

    added wave effect to begginging

--- Waves-IUGO-ggj17/Assets/Scripts/StartScreen.cs
+++ Waves-IUGO-ggj17/Assets/Scripts/StartScreen.cs
@@ -7,16 +7,47 @@ using UnityEngine.UI;
 public class StartScreen : MonoBehaviour {
 
   Text Text;
+  WaterDistortion WaterEffect;
+  bool ButtonClicked = false;
+  float ExitTimer = 1.0f;
 
   void Awake()
   {
     Text = transform.FindChild("Score").GetComponent<Text>();
     Text.text = "Deepest: " + PlayerPrefs.GetInt("Deepest") + "m";
+
+    WaterEffect = gameObject.GetComponent<Canvas>().worldCamera.GetComponent<WaterDistortion>();
   }
 
-  public void StartGame()
+  private void Update()
   {
-    SceneManager.LoadScene("2_Gameplay");
+    if(ButtonClicked)
+    {
+      ExitTimer -= Time.deltaTime;
+      WaterEffect._Distortion += 0.5f;
+      if (ExitTimer < 0.0f)
+      {
+        SceneManager.LoadScene("2_Gameplay");
+      }
+    }
+  }
+
+  public void StartNormalGame()
+  {
+    ServiceLocator.Difficulty = EDifficulty.Normal;
+    ButtonClicked = true;
+  }
+
+  public void StartHardGame()
+  {
+    ServiceLocator.Difficulty = EDifficulty.Hard;
+    ButtonClicked = true;
+  }
+
+  public void StartNightmareGame()
+  {
+    ServiceLocator.Difficulty = EDifficulty.Nightmare;
+    ButtonClicked = true;
   }
 
   public void QuitGame()
--- Waves-IUGO-ggj17/Assets/Scripts/WaterDistortion.cs
+++ Waves-IUGO-ggj17/Assets/Scripts/WaterDistortion.cs
@@ -6,59 +6,57 @@ using UnityEngine;
 // as it is scaled to a smaller resolution.
 // The effect works by accumulating the previous frames in an accumulation
 // texture.
-namespace UnityStandardAssets.ImageEffects
+
+[RequireComponent(typeof(Camera))]
+public class WaterDistortion : MonoBehaviour
 {
-  [RequireComponent(typeof(Camera))]
-  public class WaterDistortion : MonoBehaviour
+  [Range(0.1f, 4.0f)]
+  public float _Speed = 0.5f;
+
+  [Range(0.1f, 20.0f)]
+  public float _Distortion = 5.0f;
+
+  [Range(1.0f, 100.0f)]
+  public float _Waves = 20.0f;
+
+  /// Provides a shader property that is set in the inspector
+  /// and a material instantiated from the shader
+  public Shader shader;
+
+  private Material material;
+
+  protected virtual void Start()
   {
-    [Range(0.1f, 4.0f)]
-    public float _Speed = 0.5f;
+    material = new Material(shader);
 
-    [Range(0.1f, 20.0f)]
-    public float _Distortion = 5.0f;
-
-    [Range(1.0f, 100.0f)]
-    public float _Waves = 20.0f;
-
-    /// Provides a shader property that is set in the inspector
-    /// and a material instantiated from the shader
-    public Shader shader;
-
-    private Material material;
-
-    protected virtual void Start()
+    // Disable if we don't support image effects
+    if (!SystemInfo.supportsImageEffects)
     {
-      material = new Material(shader);
-
-      // Disable if we don't support image effects
-      if (!SystemInfo.supportsImageEffects)
-      {
-        enabled = false;
-        return;
-      }
-
-      // Disable the image effect if the shader can't
-      // run on the users graphics card
-      if (!shader || !shader.isSupported)
-        enabled = false;
+      enabled = false;
+      return;
     }
 
-    void OnDisable()
-    {
-      if (material)
-      {
-        DestroyImmediate(material);
-      }
-    }
+    // Disable the image effect if the shader can't
+    // run on the users graphics card
+    if (!shader || !shader.isSupported)
+      enabled = false;
+  }
 
-    // Called by camera to apply image effect
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
+  void OnDisable()
+  {
+    if (material)
     {
-      material.SetFloat("_Speed", _Speed);      
-      material.SetFloat("_Distortion", _Distortion);
-      material.SetFloat("_Waves", _Waves);
-
-      Graphics.Blit(source, destination, material);
+      DestroyImmediate(material);
     }
+  }
+
+  // Called by camera to apply image effect
+  void OnRenderImage(RenderTexture source, RenderTexture destination)
+  {
+    material.SetFloat("_Speed", _Speed);
+    material.SetFloat("_Distortion", _Distortion);
+    material.SetFloat("_Waves", _Waves);
+
+    Graphics.Blit(source, destination, material);
   }
 }
