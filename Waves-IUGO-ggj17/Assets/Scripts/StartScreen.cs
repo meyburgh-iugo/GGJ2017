54728ee41915a7d379c3ae11c86a87c283ff8dac
Brett Cundal <brett.cundal@iugome.com>
2018-01-24 22:30:14 -0800

    Automatic upgrade to 2017.3

--- Waves-IUGO-ggj17/Assets/Scripts/StartScreen.cs
+++ Waves-IUGO-ggj17/Assets/Scripts/StartScreen.cs
@@ -14,7 +14,7 @@ public class StartScreen : MonoBehaviour {
 
   void Awake()
   {
-    Text = transform.FindChild("Score").GetComponent<Text>();
+    Text = transform.Find("Score").GetComponent<Text>();
     Text.text = string.Format("Deepest distances:\nNormal: {0} m\nHard: {1} m\nNightmare: {2} m", PlayerPrefs.GetInt("Deepest_Normal"), PlayerPrefs.GetInt("Deepest_Hard"), PlayerPrefs.GetInt("Deepest_Nightmare"));
 
     CanvasCamera = gameObject.GetComponent<Canvas>().worldCamera;
--- Waves-IUGO-ggj17/Assets/Shaders/WaterDistortionShader.shader
+++ Waves-IUGO-ggj17/Assets/Shaders/WaterDistortionShader.shader
@@ -1,4 +1,6 @@
-﻿Shader "IUGOGGJ17/WaterDistortion" {
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "IUGOGGJ17/WaterDistortion" {
 	Properties{
 		_MainTex("Base (RGB)", 2D) = "white" {}
 	}
@@ -27,7 +29,7 @@
 	v2f vert(appdata_img v)
 	{
 		v2f o;
-		o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+		o.pos = UnityObjectToClipPos(v.vertex);
 		o.uv = v.texcoord;
 		return o;
 	}
